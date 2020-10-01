fc886e088bb6de3424266227ba7ecafde8d21efa
Troy Dugger <tdugger@us.ibm.com>
2020-09-30 23:07:09 -0500

    switch to using express

--- assistant-proxy.js
+++ assistant-proxy.js
@@ -1,19 +1,39 @@
-const http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-   if (req.method === "GET") {
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+    console.log("Got a GET request for the homepage");
+    res.send('Hello GET');
+});
 
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("hi Troy");
+// This responds a POST request for the homepage
+app.post('/', function (req, res) {
+    console.log("Got a POST request for the homepage");
+    res.send('Hello POST');;
+});
 
-   } else if (req.method === "POST") {
+// This responds a DELETE request for the /del_user page.
+app.delete('/del_user', function (req, res) {
+    console.log("Got a DELETE request for /del_user");
+    res.send('Hello DELETE');
+});
 
-      req.on("end", function () {
-         res.writeHead(200, { "Content-Type": "application/json" });
-         res.end({ akey: "avalue", bkey: "bvalue" });
-      });
+// This responds a GET request for the /list_user page.
+app.get('/list_user', function (req, res) {
+    console.log("Got a GET request for /list_user");
+    res.send('Page Listing');
+});
 
-   }
-}).listen(8080);
+// This responds a GET request for abcd, abxcd, ab123cd, and so on
+app.get('/ab*cd', function (req, res) {
+    console.log("Got a GET request for /ab*cd");
+    res.send('Page Pattern Match');
+});
 
-console.log('Server running at http://0.0.0.0:8080/');
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+   
+    console.log("Example app listening at http://%s:%s", host, port)
+});
