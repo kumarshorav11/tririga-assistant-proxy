2e25f21f9c1453d9fd67f96535d4c74605f8bae5
Troy Dugger <tdugger@us.ibm.com>
2020-09-30 20:30:22 -0500

    return a JSON obj

--- assistant-proxy.js
+++ assistant-proxy.js
@@ -1,9 +1,10 @@
 const http = require('http');
 
 http.createServer(function (request, response) {
-   target = process.env.TARGET ? process.env.TARGET : 'World' ;
-   msg = process.env.MSG ? process.env.MSG : 'Hello ' + target + '\n';
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+   //target = process.env.TARGET ? process.env.TARGET : 'World' ;
+   //msg = process.env.MSG ? process.env.MSG : 'Hello ' + target + '\n';
+   msg = { akey: "value", bkey: "value2" };
+   response.writeHead(200, {'Content-Type': 'application/json'});
    response.end(msg);
 }).listen(8080);
 
