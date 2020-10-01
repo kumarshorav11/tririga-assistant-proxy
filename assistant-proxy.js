679bf4a42c203452300d0d9ed28a335b6775024d
Troy Dugger <tdugger@us.ibm.com>
2020-09-30 21:48:32 -0500

    get and post

--- assistant-proxy.js
+++ assistant-proxy.js
@@ -1,11 +1,19 @@
 const http = require('http');
 
-http.createServer(function (request, response) {
-   //target = process.env.TARGET ? process.env.TARGET : 'World' ;
-   //msg = process.env.MSG ? process.env.MSG : 'Hello ' + target + '\n';
-   msg = { akey: "value", bkey: "value2" };
-   response.writeHead(200, {'Content-Type': 'application/json'});
-   response.end(msg);
+http.createServer(function (req, res) {
+   if (req.method === "GET") {
+
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("hi Troy");
+
+   } else if (req.method === "POST") {
+
+      req.on("end", function () {
+         res.writeHead(200, { "Content-Type": "application/json" });
+         res.end({ akey: "avalue", bkey: "bvalue" });
+      });
+
+   }
 }).listen(8080);
 
 console.log('Server running at http://0.0.0.0:8080/');
