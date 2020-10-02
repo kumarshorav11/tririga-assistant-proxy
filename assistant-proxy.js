7dfb8470acc7be1d054d64c0398f37d220aeb5ed
Troy Dugger <tdugger@us.ibm.com>
2020-10-02 18:56:35 -0500

    log request body

--- assistant-proxy.js
+++ assistant-proxy.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
 
 // This responds a POST request for the homepage
 app.post('/', function (req, res) {
-    console.log("Got a POST request for the homepage");
+    console.log("Got a POST request", req.body);
     res.json(req.body);
 });
 
