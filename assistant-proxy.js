f265c00ad815b7368be1e354c44fdf93fd753989
Troy Dugger <tdugger@us.ibm.com>
2020-10-02 18:22:19 -0500

    change post handling to return json body

--- assistant-proxy.js
+++ assistant-proxy.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 // This responds a POST request for the homepage
 app.post('/', function (req, res) {
     console.log("Got a POST request for the homepage");
-    res.send('Hello POST');;
+    res.json(req.body);
 });
 
 // This responds a DELETE request for the /del_user page.
